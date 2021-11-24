ff8e85fb4b2aca0ed6ba364f6954efb6e230514d
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-24 13:36:57 -0700

    call method

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def sysinfo():
 def fetch():
     print('updating from the repo')
     command = 'git fetch https://github.com/Smith-Chris1/pi-dash.git'.split()
-    subprocess.check_output(command, cwd='/home/pi/pi-dash', shell=True)
+    subprocess.call(command, cwd='/home/pi/pi-dash', shell=True)
     return 'success'
 
 @app.route('/scan')
