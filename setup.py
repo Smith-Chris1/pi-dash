ac085fede925dd226f42dc2f9e8fe1f88d079aab
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-25 09:46:06 -0700

    added a try catch for the installer

--- setup.py
+++ setup.py
@@ -3,33 +3,35 @@ import subprocess
 import shutil
 import os
 
-print("Setting up Temple Pi")
 
-CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-print(CURR_DIR)
+try:
+    print("Setting up Temple Pi")
 
-path = '/home/pi/pi-dash'
+    CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+    print(CURR_DIR)
 
-isExist = os.path.exists(path)
+    path = '/home/pi/pi-dash'
 
-if not isExist:
-    os.makedirs(path)
-    print("The new directory is created!")
+    isExist = os.path.exists(path)
 
-print('updating from the repo')
-process = subprocess.Popen(["git", "-C",path,"pull", "https://github.com/Smith-Chris1/pi-dash.git"], stdout=subprocess.PIPE)
-output = process.communicate()[0]
+    if not isExist:
+        os.makedirs(path)
+        print("The new directory is created!")
+
+    print('updating from the repo')
+    process = subprocess.Popen(["git", "-C",path,"pull", "https://github.com/Smith-Chris1/pi-dash.git"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
 
 
-print("installing dependancies")
-process = subprocess.Popen([ 'pip', 'install', '-r', path+'/requirements.txt' ], cwd=path, stdout=subprocess.PIPE)
-output = process.communicate()[0]
+    print("installing dependancies")
+    process = subprocess.Popen([ 'pip', 'install', '-r', path+'/requirements.txt' ], cwd=path, stdout=subprocess.PIPE)
+    output = process.communicate()[0]
 
-print('moving files')
-shutil.copyfile(path+'/vlc/temple.html', '/usr/share/vlc/lua/http/temple.html')
+    print('moving files')
+    shutil.copyfile(path+'/vlc/temple.html', '/usr/share/vlc/lua/http/temple.html')
 
-print('cleaning up')
-if CURR_DIR != path:
+    print('cleaning up')
+    if CURR_DIR != path:
+        os.remove(__file__)
+except:
     os.remove(__file__)
-
-
