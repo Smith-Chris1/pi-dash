0b66d9c3368fc6deb9991b63cac0bd10ebd838ba
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-25 09:23:49 -0700

    fixing typos

--- setup.py
+++ setup.py
@@ -10,28 +10,26 @@ print(CURR_DIR)
 
 path = '/home/pi/pi-dash'
 
-# Check whether the specified path exists or not
 isExist = os.path.exists(path)
 
 if not isExist:
-    # Create a new directory because it does not exist 
     os.makedirs(path)
     print("The new directory is created!")
 
 print('updating from the repo')
-process = subprocess.Popen(["git", "-C","/home/pi/pi-dash","pull", "https://github.com/Smith-Chris1/pi-dash.git"], stdout=subprocess.PIPE)
+process = subprocess.Popen(["git", "-C",path,"pull", "https://github.com/Smith-Chris1/pi-dash.git"], stdout=subprocess.PIPE)
 output = process.communicate()[0]
 
 
 print("installing dependancies")
-pythoninstall = subprocess.Popen([ 'pip', 'install', '-r', '/home/pi/pi-dashrequirements.txt' ], cwd="/home/pi/pi-dash", stdout=subprocess.PIPE, shell=True)
+pythoninstall = subprocess.Popen([ 'pip', 'install', '-r', path+'/requirements.txt' ], cwd=path, stdout=subprocess.PIPE, shell=True)
 pythoninstall.communicate[0]
 
 print('moving files')
-shutil.copyfile('/home/pi/pi-dash/vlc/temple.html', '/usr/share/vlc/lua/http/temple.html')
+shutil.copyfile(path+'/vlc/temple.html', '/usr/share/vlc/lua/http/temple.html')
 
 print('cleaning up')
-if CURR_DIR is not '/hom/pi/pi-dash':
+if CURR_DIR != path:
     os.remove(__file__)
 
 
