3cf636d1262f25a00255c1921f112aa8f154fe19
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-26 10:15:13 -0700

    print

--- app.py
+++ app.py
@@ -126,7 +126,9 @@ def scan():
         print(pi)
         info = pi.split(' ')
         print(info)
+        print('regex: ' + re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[1])
         try:
+            
             if info[1] == re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[1].replace(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[1], "1"):
             # if info[1].replace("(", "").replace(")","") == info[1].replace("(", "").replace(")","").
             # r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})"
