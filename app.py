95720d0496972ec897e05385c78b2fd5f7174d6b
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-26 10:19:36 -0700

    wrong regex

--- app.py
+++ app.py
@@ -130,7 +130,7 @@ def scan():
         print('regex: ' + re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[0])
         try:
             
-            if info[1] == re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[0].replace(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[1], "1"):
+            if info[1] == re.findall(r"\((.*?)\)", info[1])[0].replace(re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})", info[1])[1], "1"):
             # if info[1].replace("(", "").replace(")","") == info[1].replace("(", "").replace(")","").
             # r"\d{1,3}\.\d{1,3}\.\d{1,3}\.(\d{1,3})"
             # r"\((.*?)\)"
