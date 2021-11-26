ea7b1905252b7d1fe22e76b9104a0b18c0a78896
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-11-26 11:41:20 -0700

    try instead of if

--- app.py
+++ app.py
@@ -119,9 +119,10 @@ def scan():
                     sysinfo = requests.request('POST','http://'+re.findall(r"\((.*?)\)", info[1])[0]+':5000/sysinfo').text.split(",")
                     try:
                         # if sysinfo[0] not in scans:
-                        if requests.request('GET','http://'+re.findall(r"\((.*?)\)", info[1])[0]+':8080').status_code != 200:
-                            iframe = render_template('startVLC.html')
-                        else:
+                        try:
+                            if requests.request('GET','http://'+re.findall(r"\((.*?)\)", info[1])[0]+':8080').status_code != 200:
+                                iframe = render_template('startVLC.html')
+                        except:
                             iframe = '<iframe src="http://{{ ip }}:8080/temple.html" style="min-width:308px; min-height: 205px;"></iframe>'
                         scans.append(render_template('card.html', 
                             host = sysinfo[0],
