81d322da4a2b0e7c2ddc9cd672dd00ea1ab9e42c
Smith-Chris1 <christopher.erick.smith@gmail.com>
2021-12-06 12:42:51 -0700

    remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from getpass import getpass
-from subprocess import Popen, PIPE
-
-command = 'shutdown -r now'.split()
-
-p = Popen(['sudo', '--stdin'] + command, stdin=PIPE, stderr=PIPE,
-          universal_newlines=True)
-sudo_prompt = p.communicate('WestonDean=0219!\n')[1]
