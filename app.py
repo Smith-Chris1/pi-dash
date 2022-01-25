2eba9bf15ad242bf596a29d3417ffd22b718fc87
Smith-Chris1 <christopher.erick.smith@gmail.com>
2022-01-25 14:33:04 -0700

    changed the socket port

--- app.py
+++ app.py
@@ -321,4 +321,5 @@ def vlcUp(ip):
     return result
 
 if __name__ == '__main__':
-    socketio.run(app,host='0.0.0.0', debug=True, port=5000)
+    app.run(port=5000)
+    socketio.run(app,host='0.0.0.0', debug=True, port=5005)
