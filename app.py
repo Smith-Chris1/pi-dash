1718a59bff54611e42b6f675d4a243e2bc63ec0e
Smith-Chris1 <christopher.erick.smith@gmail.com>
2022-01-25 13:23:54 -0700

    novlc up

--- app.py
+++ app.py
@@ -136,7 +136,8 @@ def load_one(message):                        # test_message() is the event call
         else:
             thisInfo.strip()
         print(thisInfo)
-        vlc = vlcUp(thisInfo)
+        # vlc = vlcUp(thisInfo)
+        vlc = 0
         print(vlc)
         if vlc == 0:
             iframe = '<iframe src="http://' + thisInfo + ':8080/table.html" style="min-width:320px; max-height: 50px;"></iframe>'
