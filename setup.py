06ab26985104abc5209e178f00e0cf9244d4d681
pictorycs <christopher.erick.smith@gmail.com>
2024-02-12 22:30:46 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ try:
         subprocess.Popen(['rm', '-r', path])
         time.sleep(10)
         os.makedirs(path)
-        os.makedirs(path+'/dockerStuff')
+        # os.makedirs(path+'/dockerStuff')
 
     
     # if Path(path+'requirements.txt').is_file():
