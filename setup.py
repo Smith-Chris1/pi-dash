5a37b7fd77d794994631386987480b36c7f57f65
pictorycs <christopher.erick.smith@gmail.com>
2024-02-13 19:45:05 -0500

    permissions test

--- setup.py
+++ setup.py
@@ -70,6 +70,8 @@ try:
         print("installing docker")
         process = subprocess.Popen(['sudo', 'apt', 'install', 'docker.io'], stdout=subprocess.PIPE)
         process.communicate()[0]
+    process = subprocess.Popen(['sudo', 'usermod', '-aG', 'docker', '$USER'], stdout=subprocess.PIPE)
+    process.communicate()[0]
     # subprocess.Popen(['sh', path+'/scripts/docker.sh'])
     # subprocess.Popen(['sudo', 'usermod', '-aG', 'docker', 'pi', '&&', 'sudo', 'usermod', '-aG', 'docker', 'Pi'])
     # subprocess.Popen(['pip3', 'install', 'docker'])
